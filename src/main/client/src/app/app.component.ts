105528bdccfb33f4e6b08d28d99c5530d561a58b
Dave Syer <dsyer@pivotal.io>
2017-10-26 17:36:24 +0100

    Add http backend data

--- src/main/client/src/app/app.component.ts
+++ src/main/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  data = {};
+  constructor(private http: HttpClient) {
+    http.get('resource').subscribe(data => {this.data = data;});
+  }
 }
