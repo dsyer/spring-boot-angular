c246f5616b0eeaa0fb8cbf390542a7faff740298
Dave Syer <dsyer@vmware.com>
2021-12-20 15:19:37 +0000

    Move http controller into test method

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,8 +11,6 @@ describe('AppComponent', () => {
       ],
       imports: [HttpClientTestingModule]
     }).compileComponents();
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -32,7 +27,7 @@ describe('AppComponent', () => {
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const req = httpTestingController.expectOne('resource');
+    const req = TestBed.inject(HttpTestingController).expectOne('resource');
     expect(req.request.method).toEqual('GET');
     req.flush({ "id": "1234", "content": "Hello" });
     fixture.whenRenderingDone().then(
